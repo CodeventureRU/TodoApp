79607e09495de95e70541e1c04e9a6651417bf28
Ante <coderasylum@gmail.com>
2023-06-06 15:19:04 +0300

    feat: добавить базу данных

--- server/src/project/project/settings.py
+++ server/src/project/project/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'API.user'
 
 # Application definition
 
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ToDoAppDB',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
