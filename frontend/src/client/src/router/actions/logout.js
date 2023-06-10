3e3df3bc44e24d75ef92497e8bf01518d20967b2
Лебедев Александр <god@lifegame.su>
2023-06-10 21:50:00 +0300

    feat: реализация функции выхода

--- frontend/src/client/src/router/actions/logout.js
+++ frontend/src/client/src/router/actions/logout.js
@@ -1,9 +1,9 @@
-// import {logout} from "../../store/features/userSlice";
+import {logout} from "../../store/features/auth/authSlice";
 import {redirect} from "react-router-dom";
-// import {store} from "../../store";
+import {store} from "../../store";
 
 
 export default function logoutAction () {
-    // store.dispatch(logout());
+    store.dispatch(logout());
     return redirect("/login");
 }
