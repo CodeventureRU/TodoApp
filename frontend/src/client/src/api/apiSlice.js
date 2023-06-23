44783a396ee2efac7005c7dd8f81183479c1f948
Лебедев Александр <god@lifegame.su>
2023-06-23 12:59:18 +0300

    fix(api): исправление базового запроса
    Исправление базового запроса и механизма обновления токена в случае истечения срока его действия.

--- frontend/src/client/src/api/apiSlice.js
+++ frontend/src/client/src/api/apiSlice.js
@@ -1,11 +1,11 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {logout, setToken} from "../store/features/auth/authSlice";
 import env from "react-dotenv";
+import {store} from "../store";
 
 
 const baseQuery = fetchBaseQuery({
     baseUrl: env.REACT_APP_API_URL + "/api/v1",
-    credentials: 'include',
     prepareHeaders: (headers, {getState}) => {
         const token = getState().auth.accessToken;
         if (token) {
@@ -18,14 +18,22 @@ const baseQuery = fetchBaseQuery({
 const baseQueryWithRefresh = async (args, api, extraOptions) => {
     let result = await baseQuery(args, api, extraOptions);
 
-    if (result?.error?.originalStatus === 403) {
-        console.log('Sending refresh');
+    if (result?.error?.status === 401) {
 
-        // Если ошибка 403, то отправляем запрос на обновление access-токена
-        const refreshResult = await baseQuery("/user/refresh", api, extraOptions);
-        if (refreshResult?.data) {
+        // Если ошибка 401, то отправляем запрос на обновление access-токена
+        const refreshResult = await fetch(env.REACT_APP_API_URL + "/api/v1/user/refresh/", {
+            method: "POST",
+            body: JSON.stringify({"refresh": store.getState().auth.refreshToken}),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (refreshResult?.access) {
             // Сохранение нового токена
-            api.dispatch(setToken(refreshResult.data));
+            api.dispatch(setToken({
+                access: refreshResult.access,
+                refresh: store.getState().auth.refreshToken
+            }));
 
             // Повторение изначального запроса
             result = await baseQuery(args, api, extraOptions);
