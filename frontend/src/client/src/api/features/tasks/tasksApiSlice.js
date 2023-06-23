25f060101caab9f7c101e0c392279495c29aad50
Лебедев Александр <god@lifegame.su>
2023-06-23 13:00:54 +0300

    feat: добавления слайса для списка задач
    Добавление API-слайса для создания, получения и удаления списков задач.

---
+++ frontend/src/client/src/api/features/tasks/tasksApiSlice.js
@@ -0,0 +1,46 @@
+import {apiSlice} from "../../apiSlice";
+
+
+export const tasksApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+
+
+
+        getTasks: builder.query({
+            query: _ => ({
+                url: '/tasks/',
+                method: 'GET',
+            }),
+            providesTags: ['Tasks']
+        }),
+
+        getLists: builder.query({
+            query: _ => ({
+                url: '/lists/',
+                method: 'GET',
+                tags: ['Lists'],
+            }),
+            providesTags: ['Lists']
+        }),
+
+        createList: builder.mutation({
+            query: data => ({
+                url: '/lists/',
+                method: 'POST',
+                body: {...data},
+            }),
+            invalidatesTags: ['Lists']
+        }),
+
+        removeList: builder.mutation({
+            query: (id) => ({
+                url: `/lists/${id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ["Lists"]
+        }),
+
+    })
+});
+
+export const {useGetTasksQuery, useGetListsQuery, useCreateListMutation, useRemoveListMutation} = tasksApiSlice;
